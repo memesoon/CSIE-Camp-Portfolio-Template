df6d57314982ad48f79b5608b2951fba0b1ff905
memesoon <108781626+memesoon@users.noreply.github.com>
2022-07-07 16:12:40 +0800

    changing profile

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -51,7 +51,7 @@ const links = [
                 class="absolute top-4 left-4 z-50 h-20 w-20 overflow-hidden rounded-full bg-pink-200 shadow-md shadow-blue-300 ring-1 ring-violet-700 md:h-24 md:w-24"
                 v-if="$route.path !== '/about'"
             >
-                <img src="https://placekitten.com/320/320" class="w-full rounded-full" />
+                <img src="https://lh3.googleusercontent.com/a-/AFdZucphH8dmBVUMnFWNd2tEz3A7A61Kjwxx1CLjToKMWQ=s288-p-rw-no" class="w-full rounded-full" />
             </div>
         </transition>
     </div>
